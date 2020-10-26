e040caf9f7203edc8753313152463191b6467a19
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-10-26 10:02:10 +0100

    3 commit

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = cidrsubnet(var.network_address_space, 8, 1)
+  address_prefixes     = ["cidrsubnet(var.network_address_space, 8, 1)"]
 }
 
 resource "azurerm_network_interface" "main" {
@@ -32,47 +32,84 @@ resource "azurerm_network_interface" "main" {
   resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+
+###### NEW
+
+resource "azurerm_linux_virtual_machine" "main" {
   count                 = var.web_node_count
   name                  = format("${var.prefix}-%02d", count.index + 1)
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index +1)]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_F2"
+  admin_username      = "ubuntu"
+  network_interface_ids = [
+    element(azurerm_network_interface.main.*.id, count.index +1),
+  ]
   delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
   delete_data_disks_on_termination = true
 
-  storage_image_reference {
+
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = var.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = format("${var.prefix}-disk-%02d", count.index + 1)
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = format("${var.prefix}-%02d", count.index + 1)
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
 }
+
+
+###### OLD
+#resource "azurerm_virtual_machine" "main" {
+#  count                 = var.web_node_count
+#  name                  = format("${var.prefix}-%02d", count.index + 1)
+#  location              = azurerm_resource_group.main.location
+#  resource_group_name   = azurerm_resource_group.main.name
+#  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index +1)]
+#  vm_size               = "Standard_DS1_v2"
+#
+#  # Uncomment this line to delete the OS disk automatically when deleting the VM
+#  delete_os_disk_on_termination = true
+#
+#  # Uncomment this line to delete the data disks automatically when deleting the VM
+#  delete_data_disks_on_termination = true
+#
+#  storage_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#  }
+#  storage_os_disk {
+#    name              = format("${var.prefix}-disk-%02d", count.index + 1)
+#    caching           = "ReadWrite"
+#    create_option     = "FromImage"
+#    managed_disk_type = "Standard_LRS"
+#  }
+#  os_profile {
+#    computer_name  = format("${var.prefix}-%02d", count.index + 1)
+#    admin_username = var.admin_username
+#    admin_password = var.admin_password
+#  }
+#  os_profile_linux_config {
+#    disable_password_authentication = false
+#  }
+#  tags = {
+#    environment = "staging"
+#  }
+#}
