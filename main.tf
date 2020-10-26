b754a3853168d8dc231d25ffc99d59b756fb181f
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-10-26 10:07:46 +0100

    5 commit

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["cidrsubnet(var.network_address_space, 8, 1)"]
+  address_prefixes     = [cidrsubnet(var.network_address_space, 8, 1),]
 }
 
 resource "azurerm_network_interface" "main" {
