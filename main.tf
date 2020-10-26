e5a7627ccb79a0165bfa7f84d0198e5d99e14124
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-10-26 10:49:18 +0100

    9 commit

--- main.tf
+++ main.tf
@@ -42,6 +42,34 @@ resource "azurerm_network_interface" "main" {
 ### bastion host
 ####
 
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.internal.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+
+
+
+
+
 resource "azurerm_public_ip" "example" {
   count               = var.web_node_count
   name                = format("publicip-%02d", count.index + 1)
