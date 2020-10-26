4decef09aaa0acc128ff2804bc42b0da9bc4c968
Jörn Stenkamp <joern@Jorns-MacBook-Pro.fritz.box>
2020-10-26 10:21:38 +0100

    6 commit

--- main.tf
+++ main.tf
@@ -39,7 +39,30 @@ resource "azurerm_network_interface" "main" {
 }
 
 
-###### NEW
+### bastion host
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "bastion"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "main" {
+  name                = "bastion"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.main.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
+
+
+###### NEW webnode workers
 
 resource "azurerm_linux_virtual_machine" "main" {
   count                 = var.web_node_count
